bbdf79521a49460b82e04022bfc0c8e82b766e38
vanthaohuynh <86531871+vanthaohuynh@users.noreply.github.com>
2022-10-21 11:45:30 -0400

    fix17

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_dovee', secure: true, domain: 'dovee-finance.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: '_dovee', domain: 'dovee-finance.herokuapp.com'
 else
-  Rails.application.config.session_store :cookie_store, key: '_dovee', secure: true
+  Rails.application.config.session_store :cookie_store, key: '_dovee'
 end

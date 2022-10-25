8db6cf89dc83e5a2092187eda83c2faa7e93d108
vanthaohuynh <86531871+vanthaohuynh@users.noreply.github.com>
2022-10-25 14:06:34 -0400

    nothing changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,41 @@
 class ApplicationController < ActionController::Base
+  # This is for setting app installed with --api flag
+  # class ApplicationController < ActionController::API
+  # before_action :set_csrf_cookie
+  # include ActionController::Cookies
+  # include ActionController::RequestForgeryProtection
+
+  # protect_from_forgery with: :exception
+
+  # def cookie
+  #   'ok'
+  # end
+
+  # private
+
+  # def set_csrf_cookie
+  #   cookies['CSRF-TOKEN'] = {
+  #     value: form_authenticity_token,
+  #     domain: :all,
+  #     same_site: :none,
+  #     # secure: Rails.env.production?
+  #     secure: true
+  #   }
+  # end
+
   skip_before_action :verify_authenticity_token
   # protect_from_forgery with: :null_session // Come back to this later.
   # Right now cannot activate this line. The app will not work with it.
 
   # before_action :set_current_user
 
+  # This one is working fine (in local)
   def current_user
-    # finds user with session data and stores it if present
     @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
 
+  # This code was in the template
   # def authorize
   #   redirect_to login_path, alert: 'Not authorized - you must be logged in!' if current_user.nil?
   # end
--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,11 @@ module Finance
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # config.api_only = true
+
+    # config.middleware.use ActionDispatch::Cookies
+    # config.middleware.use ActionDispatch::Session::CookieStore
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

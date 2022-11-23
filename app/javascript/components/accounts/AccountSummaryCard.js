efa2f6e2cd11cba3dfc5a9807572b9eb51aa033a
vanthaohuynh <86531871+vanthaohuynh@users.noreply.github.com>
2022-11-23 11:02:11 -0500

    working fine

--- app/javascript/components/accounts/AccountSummaryCard.js
+++ app/javascript/components/accounts/AccountSummaryCard.js
@@ -2,30 +2,51 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
+import PropTypes from 'prop-types';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-const AccountSummaryCard = () => {
-  const something = 'something';
+const AccountSummaryCard = ({ cardTransactionData }) => {
+  const revenueTransactions = cardTransactionData.filter((transaction) => transaction.transaction_type === 'Revenue');
+  const expenseTransactions = cardTransactionData.filter((transaction) => transaction.transaction_type === 'Expense');
+  const revenueTotal = revenueTransactions
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+  const revenueOverHeadTotal = revenueTransactions
+    .reduce((acc, transaction) => acc + transaction.overhead, 0);
+  const revenueAfterOverHeadTotal = revenueTotal - revenueOverHeadTotal;
+  const expenseTotal = expenseTransactions
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+  const balance = revenueTotal - expenseTotal;
+  // console.log('revenueTotal', revenueTotal);
+  // console.log('expenseTotal', expenseTotal);
+  // console.log('balance', balance);
 
   return (
     <Card sx={{ minWidth: 250 }}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
           Account Summary
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          Account Name:
+          Revenues: $
+          {revenueTotal}
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          Revenues:
+          Overhead: $
+          {revenueOverHeadTotal}
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          Expenses:
+          After Overhead: $
+          {revenueAfterOverHeadTotal}
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          Balance:
+          Expenses: $
+          {expenseTotal}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Balance: $
+          {balance}
         </Typography>
       </CardContent>
       {/* <CardActions>
@@ -33,6 +54,18 @@ const AccountSummaryCard = () => {
       </CardActions> */}
     </Card>
   );
+};
+
+AccountSummaryCard.propTypes = {
+  cardTransactionData: PropTypes.arrayOf(PropTypes.shape({
+    account_num: PropTypes.string,
+    invoice_num: PropTypes.string,
+    invoice_date: PropTypes.string,
+    transaction_type: PropTypes.string,
+    category: PropTypes.string,
+    amount: PropTypes.number,
+    currency: PropTypes.string,
+  })).isRequired,
 };
 
 export default AccountSummaryCard;

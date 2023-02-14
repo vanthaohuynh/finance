75830d6597487004bbbe8e241be13ab6fbe62d05
vanthaohuynh <86531871+vanthaohuynh@users.noreply.github.com>
2023-02-14 09:56:36 -0500

    changes in cors and session_sotre

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_vanthao', domain: 'vanthao.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: '_dovee-finance', domain: 'dovee-finance.herokuapp.com'
 else
-  Rails.application.config.session_store :cookie_store, key: '_vanthao'
+  Rails.application.config.session_store :cookie_store, key: '_dovee-finance'
 end
